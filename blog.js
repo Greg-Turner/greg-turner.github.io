08f2ed1be496bd00ccd62fdaf2e3878fee399960
Greg Turner <31353577+Greg-Turner@users.noreply.github.com>
2017-10-10 15:58:36 -0500

    added functionality to blog page

--- blog.js
+++ blog.js
@@ -1,3 +1,12 @@
+function readBlog(entry) {
+    var x = document.getElementById(entry);
+    if (x.style.display == "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
 // define the blog array, which will become the database
 let blog = []
 
