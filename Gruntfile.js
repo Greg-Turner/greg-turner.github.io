99a7f1e8d82b5b3bb81a9e56635161df4466011b
Greg Turner <31353577+Greg-Turner@users.noreply.github.com>
2017-11-17 13:52:36 -0600

    fixed eslint npm with gruntify-eslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,56 +1,56 @@
 module.exports = function(grunt) {
     
-        // Project configuration.
-        grunt.initConfig({
-            pkg: grunt.file.readJSON("package.json"),
-            watch: {
-                scripts: {
-                  files: ["scripts/*.js"],
-                  tasks: ["uglify"],
-                  options: {
-                    spawn: false
-                  }
-                }
-            },
-            uglify: {
-                options: {
-                    banner: "/*! <%= pkg.name %> <%= grunt.template.today(\"yyyy-mm-dd\") %> */\n"
-                },
-                build: {
-                    files: [
-{
-                        expand: true,
-                        cwd: "scripts",
-                        src: "*.js",
-                        dest: "build",
-                        ext: ".min.js"
-                    }
-]
-                }
-            },
-            eslint: {
-                all: ["scripts/*.js"],
-                options: {
-                  config: "conf/eslint.json",
-                  rulesDir: "conf/rules"
-                }
-              }
-        });
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON("package.json"),
+		watch: {
+			scripts: {
+				files: ["scripts/*.js"],
+				tasks: ["uglify"],
+				options: {
+					spawn: false
+				}
+			}
+		},
+		uglify: {
+			options: {
+				banner: "/*! <%= pkg.name %> <%= grunt.template.today(\"yyyy-mm-dd\") %> */\n"
+			},
+			build: {
+				files: [
+					{
+						expand: true,
+						cwd: "scripts",
+						src: "*.js",
+						dest: "build",
+						ext: ".min.js"
+					}
+				]
+			}
+		},
+		eslint: {
+			all: ["scripts/*.js"],
+			options: {
+				config: "eslintrc.json",
+				rulesDir: "conf/rules"
+			}
+		}
+	});
     
-        // Load the plugin that provides the tasks.
-        grunt.loadNpmTasks("grunt-contrib-uglify");
-        grunt.loadNpmTasks("grunt-contrib-watch");
-        grunt.loadNpmTasks("grunt-browserify");
-        grunt.loadNpmTasks("eslint");
+	// Load the plugin that provides the tasks.
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-contrib-watch");
+	grunt.loadNpmTasks("grunt-browserify");
+	grunt.loadNpmTasks("gruntify-eslint");
         
         
     
-        // Default task(s).
-        grunt.registerTask("default", [
-        "uglify",
-        "watch",
-        "browserify",
-        "eslint"
-        ]);
+	// Default task(s).
+	grunt.registerTask("default", [
+		"uglify",
+		"watch",
+		"browserify",
+		"eslint"
+	]);
             
-    };
+};
